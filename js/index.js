293874b9c3084824771f69de2ed601c06907f8c5
Rania Mohamed <raniamohamed@Ranias-MacBook-Air.local>
2023-09-16 12:25:34 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var text = window.prompt("Enter text:");
+if (text == "front" || text == "Front") {
+  console.log("Html, Css,bootstrap");
+} else if (ext == "back" || text == "Back") {
+  console.log("node, php , .net");
+} else {
+  console.log("ay 7aga");
+}
